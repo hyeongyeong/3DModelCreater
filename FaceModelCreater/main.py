736e950386bb1428f9d594310ef929f4394abeae
hyeon <kohy0329@naver.com>
2020-06-11 16:20:36 +0900

    [Style] File Path 수정

--- FaceModelCreater/main.py
+++ FaceModelCreater/main.py
@@ -3,6 +3,24 @@ import bpy
 import bmesh
 import numpy as np
 
+ModelFileName = "hee_f-output" 
+
+def setGlobalFilePath():
+    # set global file path
+    bpy.context.scene['file_path'] = {}
+    bpy.context.scene['my_obj'] = {}
+    bpy.context.scene['eyes'] = {}
+
+    bpy.context.scene['file_path']['point'] = os.getcwd() + "/input/model_point_hee.txt"
+    bpy.context.scene['file_path']['eye_tex'] = os.getcwd() + "/input/eyeball.jpg"
+    bpy.context.scene['file_path']['eye_brow_tex'] = os.getcwd() + "/input/eyebrow_left.txt"
+    bpy.context.scene['file_path']['mouth_tex'] = os.getcwd() + "/input/mouse.txt"
+    bpy.context.scene['file_path']['skin_tex'] = os.getcwd() + "/input/skin.txt"
+    bpy.context.scene['file_path']['face'] = os.getcwd() + "/input/" + ModelFileName + ".ply"
+    bpy.context.scene['file_path']['mouth_cavity'] = os.getcwd() + "/input/mouth_cavity.obj"
+
+
+             
 class main_Operator(bpy.types.Operator):
     bl_idname =  "mesh.create_model_main"
     bl_label = "Create Model"
@@ -10,28 +28,14 @@ class main_Operator(bpy.types.Operator):
         
     def execute(self,context):
         
-        # get global file path
-        model_file_name = "hee_f-output" 
+        setGlobalFilePath()
+        
 
-        bpy.context.scene['file_path'] = {}
-        bpy.context.scene['my_obj'] = {}
-        bpy.context.scene['eyes'] = {}
-
-        # bpy.context.scene['file_path']['point'] = os.getcwd() + "/input/model_point.txt"
-        bpy.context.scene['file_path']['point'] = os.getcwd() + "/../input/model_point_hee.txt"
-        bpy.context.scene['file_path']['eye_tex'] = os.getcwd() + "/../input/eyeball.jpg"
-        bpy.context.scene['file_path']['eye_brow_tex'] = os.getcwd() + "/../input/eyebrow_left.txt"
-        bpy.context.scene['file_path']['mouth_tex'] = os.getcwd() + "/../input/mouse.txt"
-        bpy.context.scene['file_path']['skin_tex'] = os.getcwd() + "/../input/skin.txt"
-        bpy.context.scene['file_path']['face'] = os.getcwd() + "/../input/" + model_file_name + ".ply"
-        bpy.context.scene['file_path']['mouth_cavity'] = os.getcwd() + "/../input/mouth_cavity.obj"
-
-            
         bpy.ops.import_mesh.ply(filepath=bpy.context.scene['file_path']['face'])
         bpy.ops.object.shade_smooth()
 
 
-        bpy.context.scene['my_obj']['ply'] = bpy.data.objects[model_file_name]
+        bpy.context.scene['my_obj']['ply'] = bpy.data.objects[ModelFileName]
 
 
         bpy.ops.mesh.add_eyes()
