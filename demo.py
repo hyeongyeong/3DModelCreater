7c013da3d343d80385ed358fe74c8fa1b29f035c
cafaddict <cafaddict93@gmail.com>
2020-06-24 22:13:02 +0900

    [DEMO] merge complete

--- demo.py
+++ demo.py
@@ -16,12 +16,7 @@ else:
     side_picture = 'seok_l'
 
 
-front_detection_path = cur_path + '/Detectlandmark/Detect-Facial-Features/'
-side_detection_path = 'Detectlandmark/Side_view_detector/'
-fitting_path = cur_path + '/3DMM-fitting/test/'
-blender_script_path = cur_path + '/3DModelCreater'
 side_view_detector.run('left_custom_weights.dat', side_picture +'.jpg')
 front_view_detector.run('shape_predictor_68_face_landmarks.dat', front_picture +'.jpg')
 fitting.run(front_picture, side_picture)
-# subprocess.run('python test.py', shell=True)
-# subprocess('python ' + side_detection_path)
+subprocess.run('blender --python /FaceModelCreater/main.py')
