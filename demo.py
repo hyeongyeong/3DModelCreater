d21fb6614ac1908f0b6397514795649a3a8097ca
cafaddict <cafaddict93@gmail.com>
2020-06-24 12:39:51 +0900

    [DEMO] working on demo

--- demo.py
+++ demo.py
@@ -17,6 +17,6 @@ side_detection_path = cur_path + '/Detectlandmark/Side_view_detector/'
 fitting_path = cur_path + '/3DMM-fitting/test/'
 blender_script_path = cur_path + '/3DModelCreater'
 subprocess.run('python ' + side_detection_path+ 'detect.py --shape-predictor ' + side_detection_path + 'left_custom_weights.dat --image '+side_picture+'.jpg', shell= True)
-# subprocess.run('python ' + front_detection_path+'/detect_face_features.py --shape-predictor '+ front_detection_path + '/shape_predictor_68_face_landmarks.dat --image ' +front_picture+'.jpg', shell= True)
+subprocess.run('python ' + front_detection_path+'/detect_face_features.py --shape-predictor '+ front_detection_path + '/shape_predictor_68_face_landmarks.dat --image ' +front_picture+'.jpg', shell= True)
 # subprocess.run('python ' + fitting_path + 'fitting_test.py '+ front_picture+' '+ side_picture)
 # subprocess.run('blender --python '+cur_path+'/main.py -o ' + cur_path + '/output -f 2', shell=True)
