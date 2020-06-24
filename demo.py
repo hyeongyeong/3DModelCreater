9d07fa3d94bc4d6594962babe1b5803b50fb0209
cafaddict <cafaddict93@gmail.com>
2020-06-24 15:31:06 +0900

    [DEMO] working on fitting

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ import os
 import sys
 import Detectlandmark.Side_view_detector.detect as side_view_detector
 import Detectlandmark.Detect_Facial_Features.detect_face_features as front_view_detector
+import MM_fitting.test.fitting_test as fitting
 
 
 cur_path = os.getcwd()
@@ -10,16 +11,16 @@ if len(sys.argv) > 2:
     front_picture =sys.argv[1]
     side_picture = sys.argv[2]
 else:
-    front_picture = 'hee_f'
-    side_picture = 'hee_r'
+    front_picture = 'seok_f'
+    side_picture = 'seok_l'
 
 
 front_detection_path = cur_path + '/Detectlandmark/Detect-Facial-Features/'
 side_detection_path = 'Detectlandmark/Side_view_detector/'
 fitting_path = cur_path + '/3DMM-fitting/test/'
 blender_script_path = cur_path + '/3DModelCreater'
-side_view_detector.run('left_custom_weights.dat', side_picture +'.jpg')
-front_view_detector.run('shape_predictor_68_face_landmarks.dat', front_picture +'.jpg')
-
+# side_view_detector.run('left_custom_weights.dat', side_picture +'.jpg')
+# front_view_detector.run('shape_predictor_68_face_landmarks.dat', front_picture +'.jpg')
+fitting.run()
 # subprocess.run('python test.py', shell=True)
 # subprocess('python ' + side_detection_path)
