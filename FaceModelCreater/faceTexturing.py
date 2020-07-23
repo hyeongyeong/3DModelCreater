1f132171e45c87aedffb89e4897d0733750674b8
KwnagheeChang <ckh916@naver.com>
2020-07-23 16:51:39 +0900

    [Feature] 텍스처 커스터마이징 함수 추가

--- FaceModelCreater/faceTexturing.py
+++ FaceModelCreater/faceTexturing.py
@@ -9,84 +9,18 @@ class MESH_OT_apply_texturing(bpy.types.Operator):
     def execute(self, context):
         control_face_roughness = 0.7 # 0.5 ~ 1 
         control_face_skin_condition = 0.1
-        texturing(self,context, control_face_roughness, control_face_skin_condition)
+        texturing(control_face_roughness, control_face_skin_condition)
+        fun_control_face_roughness(0.8)
+        
         return {'FINISHED'}
 
-def create_face_group(context,operator,group_name, control_face_roughness, control_face_skin_condition):
-         ###############################
-        bpy.context.scene.use_nodes = True
-        
-        Face_Skin_Group = bpy.data.node_groups.new(group_name,'ShaderNodeTree')
+def fun_control_face_roughness(control_face_roughness):
+    bpy.data.materials["Face"].node_tree.nodes["ColorRamp.001"].color_ramp.elements[1].position = control_face_roughness
 
-        TexCoord_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeTexCoord')
-        TexCoord_wrinkle.location = (0,0)
-        TexCoord_wrinkle.object =   bpy.context.scene['my_obj']['ply']
-        Mapping_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeMapping')
-        Mapping_wrinkle.inputs[1].default_value[0] = 0.5
-        Mapping_wrinkle.inputs[1].default_value[1] = 0.25
-        Mapping_wrinkle.inputs[3].default_value[0] = 0.009
-        Mapping_wrinkle.inputs[3].default_value[1] = 0.008
-        Mapping_wrinkle.inputs[3].default_value[2] = 0
-        Mapping_wrinkle.location = (300,0)
-        Teximage_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeTexImage')
-        Teximage_wrinkle.image = bpy.data.images.load(bpy.context.scene['file_path']['wrinkle_texture'])
-        Teximage_wrinkle.location = (600,0)
-        BrightContrast_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeBrightContrast')
-        BrightContrast_wrinkle.inputs[1].default_value = 0.15
-        BrightContrast_wrinkle.inputs[2].default_value = 1
-        BrightContrast_wrinkle.location = (900,0)
-        ColorRamp_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeValToRGB')
-        ColorRamp_wrinkle.color_ramp.elements[0].color = (0, 0, 0, 1)
-        ColorRamp_wrinkle.color_ramp.elements[0].position = 0
-        ColorRamp_wrinkle.color_ramp.elements[1].color = (1, 1, 1, 0)
-        ColorRamp_wrinkle.color_ramp.elements[1].position = 0.5
-        ColorRamp_wrinkle.location = (1200,0)
-        Math_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeMixRGB')
-        Math_wrinkle.blend_type='MULTIPLY'
-        Math_wrinkle.inputs[0].default_value = 0
-        Math_wrinkle.location = (1500,0)
-        Bump = Face_Skin_Group.nodes.new('ShaderNodeBump')
-        Bump.inputs[0].default_value = control_face_skin_condition
-        Bump.inputs[1].default_value = 0.1
-        Bump.location = (1800,150)
-        ColorRamp_roughness = Face_Skin_Group.nodes.new('ShaderNodeValToRGB')
-        ColorRamp_roughness.color_ramp.elements[0].color = (1, 1, 1, 1)
-        ColorRamp_roughness.color_ramp.elements[0].position = 0
-        ColorRamp_roughness.color_ramp.elements[1].color = (0, 0, 0, 1)
-        ColorRamp_roughness.color_ramp.elements[1].position = control_face_roughness
-        ColorRamp_roughness.location = (1800,450)
-        UVMap_face = Face_Skin_Group.nodes.new('ShaderNodeUVMap')
-        UVMap_face.uv_map = "UVMap"
-        UVMap_face.location = (900,750)
-        Teximage_face = Face_Skin_Group.nodes.new('ShaderNodeTexImage')
-        Teximage_face.image = bpy.data.images.load(bpy.context.scene['file_path']['face_texture'])
-        Teximage_face.location = (1200,750)
-        Group_input = Face_Skin_Group.nodes.new('NodeGroupInput')
-        Group_input.location = (0,450)
-        Group_output = Face_Skin_Group.nodes.new('NodeGroupOutput')
-        Group_output.location = (2100,450)
-        Face_Skin_Group.links.new(Mapping_wrinkle.inputs[0], TexCoord_wrinkle.outputs[3])
-        Face_Skin_Group.links.new(Teximage_wrinkle.inputs[0], Mapping_wrinkle.outputs[0])
-        Face_Skin_Group.links.new(BrightContrast_wrinkle.inputs[0],Teximage_wrinkle.outputs[0])
-        Face_Skin_Group.links.new(ColorRamp_wrinkle.inputs[0],BrightContrast_wrinkle.outputs[0])
-        Face_Skin_Group.links.new(Teximage_face.inputs[0],UVMap_face.outputs[0])
-        Face_Skin_Group.links.new(Group_output.inputs[0],Teximage_face.outputs[0])
-        #Face_Skin_Group.links.new(Math_wrinkle.inputs[0],Group_input.outputs[0])
-        Face_Skin_Group.links.new(BrightContrast_wrinkle.inputs[2],Group_input.outputs[0])
-        Face_Skin_Group.links.new(Math_wrinkle.inputs[1],Teximage_face.outputs[0])
-        Face_Skin_Group.links.new(Math_wrinkle.inputs[2],ColorRamp_wrinkle.outputs[0])
-        Face_Skin_Group.links.new(ColorRamp_roughness.inputs[0],Math_wrinkle.outputs[0])
-        Face_Skin_Group.links.new(Bump.inputs[0],Group_input.outputs[1])
-        Face_Skin_Group.links.new(Bump.inputs[1],Group_input.outputs[2])
-        Face_Skin_Group.links.new(Bump.inputs[2],Math_wrinkle.outputs[0])
-        Face_Skin_Group.links.new(Group_output.inputs[1],ColorRamp_roughness.outputs[0])
-        Face_Skin_Group.links.new(Group_output.inputs[2],Bump.outputs[0])
-        Face_Skin_Group.links.new(Mapping_wrinkle.inputs[1],Group_input.outputs[3])
-        Face_Skin_Group.links.new(Mapping_wrinkle.inputs[3],Group_input.outputs[4])
+def fun_control_face_skin_condition(control_face_skin_condition):
+    bpy.data.materials["Face"].node_tree.nodes["Bump"].inputs[0].default_value = control_face_skin_condition
 
-        return Face_Skin_Group
-
-def texturing(self,context, control_face_roughness, control_face_skin_condition):
+def texturing(control_face_roughness, control_face_skin_condition):
     objs = bpy.context.scene['my_obj']['ply'].data 
     f_face= open(bpy.context.scene['file_path']['skin_tex'],"r")
     face_R = []
@@ -157,20 +91,75 @@ def texturing(self,context, control_face_roughness, control_face_skin_condition)
     bpy.context.object.active_material = material_body
 
     ##############face ####################
-    group_face_texture=create_face_group(self,context,'face_texture', control_face_roughness, control_face_skin_condition)
+    
     material_face = bpy.data.materials.new("Face")
     material_face.use_nodes = True
-    face_texture_group =  material_face.node_tree.nodes.new('ShaderNodeGroup')
-    face_texture_group.node_tree = bpy.data.node_groups[group_face_texture.name]
-    face_texture_group.location = (-300,0)
-    face_texture_group.inputs[0].default_value = 1
+
+    Face_Skin_Group = material_face.node_tree
+
+    TexCoord_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeTexCoord')
+    TexCoord_wrinkle.location = (0,0)
+    TexCoord_wrinkle.object =   bpy.context.scene['my_obj']['ply']
+    Mapping_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeMapping')
+    Mapping_wrinkle.inputs[1].default_value[0] = 0.5
+    Mapping_wrinkle.inputs[1].default_value[1] = 0.25
+    Mapping_wrinkle.inputs[3].default_value[0] = 0.009
+    Mapping_wrinkle.inputs[3].default_value[1] = 0.008
+    Mapping_wrinkle.inputs[3].default_value[2] = 0
+    Mapping_wrinkle.location = (300,0)
+    Teximage_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeTexImage')
+    Teximage_wrinkle.image = bpy.data.images.load(bpy.context.scene['file_path']['wrinkle_texture'])
+    Teximage_wrinkle.location = (600,0)
+    BrightContrast_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeBrightContrast')
+    BrightContrast_wrinkle.inputs[1].default_value = 0.15
+    BrightContrast_wrinkle.inputs[2].default_value = 1
+    BrightContrast_wrinkle.location = (900,0)
+    ColorRamp_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeValToRGB')
+    ColorRamp_wrinkle.color_ramp.elements[0].color = (0, 0, 0, 1)
+    ColorRamp_wrinkle.color_ramp.elements[0].position = 0
+    ColorRamp_wrinkle.color_ramp.elements[1].color = (1, 1, 1, 0)
+    ColorRamp_wrinkle.color_ramp.elements[1].position = 0.5
+    ColorRamp_wrinkle.location = (1200,0)
+    Math_wrinkle = Face_Skin_Group.nodes.new('ShaderNodeMixRGB')
+    Math_wrinkle.blend_type='MULTIPLY'
+    Math_wrinkle.inputs[0].default_value = 0
+    Math_wrinkle.location = (1500,0)
+    Bump = Face_Skin_Group.nodes.new('ShaderNodeBump')
+    Bump.inputs[0].default_value = control_face_skin_condition
+    Bump.inputs[1].default_value = 0.1
+    Bump.location = (1800,150)
+    ColorRamp_roughness = Face_Skin_Group.nodes.new('ShaderNodeValToRGB')
+    ColorRamp_roughness.color_ramp.elements[0].color = (1, 1, 1, 1)
+    ColorRamp_roughness.color_ramp.elements[0].position = 0
+    ColorRamp_roughness.color_ramp.elements[1].color = (0, 0, 0, 1)
+    ColorRamp_roughness.color_ramp.elements[1].position = control_face_roughness
+    ColorRamp_roughness.location = (1800,450)
+    UVMap_face = Face_Skin_Group.nodes.new('ShaderNodeUVMap')
+    UVMap_face.uv_map = "UVMap"
+    UVMap_face.location = (900,750)
+    Teximage_face = Face_Skin_Group.nodes.new('ShaderNodeTexImage')
+    Teximage_face.image = bpy.data.images.load(bpy.context.scene['file_path']['face_texture'])
+    Teximage_face.location = (1200,750)
+    Face_Skin_Group.links.new(Mapping_wrinkle.inputs[0], TexCoord_wrinkle.outputs[3])
+    Face_Skin_Group.links.new(Teximage_wrinkle.inputs[0], Mapping_wrinkle.outputs[0])
+    Face_Skin_Group.links.new(BrightContrast_wrinkle.inputs[0],Teximage_wrinkle.outputs[0])
+    Face_Skin_Group.links.new(ColorRamp_wrinkle.inputs[0],BrightContrast_wrinkle.outputs[0])
+    Face_Skin_Group.links.new(Teximage_face.inputs[0],UVMap_face.outputs[0])
+    Face_Skin_Group.links.new(Math_wrinkle.inputs[1],Teximage_face.outputs[0])
+    Face_Skin_Group.links.new(Math_wrinkle.inputs[2],ColorRamp_wrinkle.outputs[0])
+    Face_Skin_Group.links.new(ColorRamp_roughness.inputs[0],Math_wrinkle.outputs[0])
+    Face_Skin_Group.links.new(Bump.inputs[2],Math_wrinkle.outputs[0])
+    
     BSDF = material_face.node_tree.nodes.get('Principled BSDF')
     BSDF.inputs[1].default_value=0
-    material_face.node_tree.links.new(BSDF.inputs[0], face_texture_group.outputs[0])
-    material_face.node_tree.links.new(BSDF.inputs[7], face_texture_group.outputs[1])
-    material_face.node_tree.links.new(BSDF.inputs[19], face_texture_group.outputs[2])
+    BSDF.location = (2100,0)
+    Face_Skin_Group.links.new(BSDF.inputs[0],Teximage_face.outputs[0])
+    Face_Skin_Group.links.new(BSDF.inputs[7],ColorRamp_roughness.outputs[0])
+    Face_Skin_Group.links.new(BSDF.inputs[19],Bump.outputs[0])
+
     bpy.context.object.active_material_index = 1
     bpy.context.object.active_material = material_face
+    
     bpy.ops.object.mode_set(mode = 'OBJECT')
 
 
